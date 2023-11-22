3fae01927a4964da3290a16bce88351b936149b7
Antor-094 <mdantor556677889@gmail.com>
2023-11-22 23:27:12 +0600

    Mongodb connection done ,server is listening on port 5000

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ async function main() {
   try {
     await mongoose.connect(config.DATABASE_URL as string);
     app.listen(config.PORT, () => {
-      console.log(`Example app listening on port ${config.PORT}`);
+      console.log(`Server is listening on port ${config.PORT}`);
     });
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
