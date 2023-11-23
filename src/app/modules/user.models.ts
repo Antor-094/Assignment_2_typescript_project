3ec95d8d47867dc419be86154abcaf6e75b8e90f
Antor-094 <mdantor556677889@gmail.com>
2023-11-23 15:14:31 +0600

    Zod validation added

--- src/app/modules/user.models.ts
+++ src/app/modules/user.models.ts
@@ -1,79 +1,80 @@
-import { Schema, model } from "mongoose";
-import { User, UserAddress, UserFullName } from "./userManagement/user.interface";
+import { Schema, model } from 'mongoose';
+import {
+  User,
+  UserAddress,
+  UserFullName,
+} from './userManagement/user.interface';
 
 const UserFullNameSchema = new Schema<UserFullName>({
-    firstName: {
-        type: String, required: true, trim: true
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    }
-})
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
 const UserAddressSchema = new Schema<UserAddress>({
-    street: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    city: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    country: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-})
+  street: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
 const userSchema = new Schema<User>({
-
-    userId: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    fullName: {
-        type: UserFullNameSchema,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    isActive: {
-        type: Boolean,
-        required: true
-    },
-    hobbies: {
-        type: [String],
-        required: true
-    },
-    address: {
-        type: UserAddressSchema,
-        required: true
-
-    }
-
-})
-
-
+  userId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: UserFullNameSchema,
+    required: [true ,'Full name is requied'],
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  hobbies: {
+    type: [String],
+    required: true,
+  },
+  address: {
+    type: UserAddressSchema,
+    required: [true,'Address is requied'],
+  },
+});
 
 export const UserModel = model<User>('user', userSchema);
--- src/app/modules/userManagement/user.interface.ts
+++ src/app/modules/userManagement/user.interface.ts
@@ -1,24 +1,21 @@
-
 export type UserFullName = {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 };
 
 export type UserAddress = {
-    street: string;
-    city: string;
-    country: string;
+  street: string;
+  city: string;
+  country: string;
 };
 export type User = {
-    userId: number;
-    username: string;
-    password: string;
-    fullName: UserFullName;
-    age: number;
-    email: string;
-    isActive: boolean;
-    hobbies: string[];
-    address: UserAddress;
+  userId: number;
+  username: string;
+  password: string;
+  fullName: UserFullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: UserAddress;
 };
-
-
